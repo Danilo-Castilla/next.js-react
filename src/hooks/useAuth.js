eb4555e490470e58e196b9d01f6a119b2c9da24c
Danilo Castilla <danilo.castilla.contreras@gmail.com>
2022-08-28 08:43:59 -0500

    Login incorporado usando Toekens Cookies

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,48 @@
+import React, { useState, useContext, createContext } from "react";
+import Cookie from 'js-cookie';
+import axios from 'axios';
+import endPoints from '@services/api/'
+
+const AuthContext = createContext();
+
+export function ProviderAuth({ children }) {
+    const auth = useProvideAuth();
+    return <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>
+}
+
+export const useAuth = () => {
+    return useContext(AuthContext)
+};
+
+function useProvideAuth() {
+    const [user, setUser] = useState(null);
+    const [error, setError]= useState();
+
+    const signIn = async (email, password) => {
+        const options = {
+            headers: {
+                accept: '*/*',
+                'Content-Type': 'application/json',
+            },
+        };
+
+        const { data: token_acces } = await axios.post(endPoints.auth.login, { email, password }, options);
+
+        if(token_acces) {
+            const token = token_acces.access_token;
+            Cookie.set('token', token, { expires: 5 });
+
+            axios.defaults.headers.Authorization = `Bearer ${token}`;
+            const { data: user } = await axios.get(endPoints.auth.profile);
+            console.log(user);
+            setUser(user);
+        }
+    };
+
+    return {
+        user,
+        error,
+        setError,
+        signIn,
+    };
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import { Fragment } from 'react';
 import { Popover, Transition } from '@headlessui/react';
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+//import { Bars3Icon, XMarkIcon } from '@heroicons/react/20/outline';
 //import Head from 'next/head';
 //import Image from 'next/image';
+
+
 
 const navigation = [
   { name: 'Product', href: '#' },
@@ -38,7 +40,10 @@ export default function Home() {
                     <div className="-mr-2 flex items-center md:hidden">
                       <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                         <span className="sr-only">Open main menu</span>
-                        <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                          <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+                        </svg>
+                        {/* <Bars3Icon className="h-6 w-6" aria-hidden="true" /> */}
                       </Popover.Button>
                     </div>
                   </div>
@@ -74,7 +79,10 @@ export default function Home() {
                     <div className="-mr-2">
                       <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                         <span className="sr-only">Close main menu</span>
-                        <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                          <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                        {/* <XMarkIcon className="h-6 w-6" aria-hidden="true" /> */}
                       </Popover.Button>
                     </div>
                   </div>
